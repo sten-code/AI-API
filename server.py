1b7af3f69c5cffa9f589e3d86a6910874c0e8196
Tim van Naarden <TimvNaarden@outlook.com>
2025-08-20 19:21:04 +0200

    Added Docker compose

--- server.py
+++ server.py
@@ -219,6 +219,6 @@ def parse_config(config: Type[Config], data: dict) -> dict | Config:
 if __name__ == "__main__":
 
     if debug:
-        app.run(debug=True)
+        app.run(debug=True, host="0.0.0.0", port=5000)
     else:
         serve(app, host="0.0.0.0", port=5000)
--- website/src/pages/ImageGen.jsx
+++ website/src/pages/ImageGen.jsx
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import '../css/ImageGen.css'
+import React, { Component } from "react";
+import "../css/ImageGen.css";
 
 const getBoolean = (targetvalue) => {
   if (targetvalue === "false") {
@@ -11,132 +11,167 @@ const getBoolean = (targetvalue) => {
   } else {
     return targetvalue;
   }
-}
+};
 
 const modelOptions = async () => {
   try {
-    const res = await fetch('http://127.0.0.1:5000/models');
+    const res = await fetch("http://127.0.0.1:5000/models");
     const data = await res.json();
-    const textGenerationModels = data['image-generation'];
-    const options = textGenerationModels.map((a) => { 
-      return ('<option name="' + a +'">' + a + "</option> "   ) 
+    const textGenerationModels = data["image-generation"];
+    const options = textGenerationModels.map((a) => {
+      return '<option name="' + a + '">' + a + "</option> ";
     });
     return options.join(" ");
   } catch (error) {
     console.error(error);
     return "";
   }
-}
+};
 
-const getResult = async (jsonData) => { 
+const getResult = async (jsonData) => {
   const res = await fetch("http://127.0.0.1:5000/image-generation/generate", {
-      method: 'POST',
-      body: jsonData,
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
+    method: "POST",
+    body: jsonData,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const data = await res.json();
-  console.log(data)
-  const generatedText = data.format.concat(data.base64)
+  console.log(data);
+  const generatedText = data.format.concat(data.base64);
   if (generatedText.constructor.name === "Array") {
-      const result = generatedText.map((a) => {
-          return a.generated_text
-      })
-      return result.join("\n")
+    const result = generatedText.map((a) => {
+      return a.generated_text;
+    });
+    return result.join("\n");
   } else {
-      return generatedText
+    return generatedText;
   }
-}
-
-
-
+};
 
 export class ImageGen extends Component {
   constructor(props) {
-    super(props)
-  
+    super(props);
+
     this.state = {
       num_inference_steps: 75,
       guidance_scale: 7.5,
       model: "runwayml/stable-diffusion-v1-5",
-      models:"",
+      models: "",
       result: "",
       seed: 42,
-      prompt: "What image do you want to generate?"
-    }
+      prompt: "What image do you want to generate?",
+    };
   }
   async componentDidMount() {
     modelOptions().then((result) => {
-        this.setState({models: result})
+      this.setState({ models: result });
     });
   }
 
   changeHandler = (e) => {
-    this.setState({[e.target.name]: getBoolean(e.target.value)})
+    this.setState({ [e.target.name]: getBoolean(e.target.value) });
 
     modelOptions().then((result) => {
-        this.setState({models: result})
+      this.setState({ models: result });
     });
+  };
 
-
-  }
-
-  submitHandler = e => { 
-    e.preventDefault()
-    const { prompt, model, seed, guidance_scale, num_inference_steps} = this.state;
-    const config = {num_inference_steps, seed, guidance_scale};
+  submitHandler = (e) => {
+    e.preventDefault();
+    const { prompt, model, seed, guidance_scale, num_inference_steps } =
+      this.state;
+    const config = { num_inference_steps, seed, guidance_scale };
     const data = { prompt, model, config };
     const jsonData = JSON.stringify(data);
     console.log(jsonData);
     getResult(jsonData).then((resultg) => {
-        this.setState({result: resultg})
-    })
-
-  }
+      this.setState({ result: resultg });
+    });
+  };
   render() {
-    const { prompt, model, seed, guidance_scale, num_inference_steps, models, result} = this.state;
+    const {
+      prompt,
+      model,
+      seed,
+      guidance_scale,
+      num_inference_steps,
+      models,
+      result,
+    } = this.state;
     return (
-      
       <div>
-        
-        <form onSubmit={this.submitHandler} className="form" >
-          
-          <div className='config'>
-            
-            <div className='input_field'>
-                Model: 
-                <select name="model" className='input-box' value={model} onChange={this.changeHandler}  dangerouslySetInnerHTML={{ __html: models }}></select>
-            </div> 
-            
-            <div className='input_field'>
-                Steps: 
-                <input type="number" name="num_inference_steps" min="25" max="999" value={num_inference_steps} onChange={this.changeHandler} className='input-box'/>
-            </div> 
-          
-            <div className='input_field'>
-                Seed: 
-                <input name="seed" type="number" className='input-box' value={seed} onChange={this.changeHandler} />
-            </div> 
-            <div className='input_field'>
-                Guidence scale: 
-                <input type="number" name="guidance_scale" className='input-box' value={guidance_scale} onChange={this.changeHandler} />
-            </div> 
-          
-          </div><br></br>
-          
-          <div className='ask'>
-            <textarea type="text" className="question" name="prompt" value={prompt} onChange={this.changeHandler} required/>
-            <div className="result"><img src={result} alt="Your generated frame here"/></div> 
-          </div><br></br>
-            
-            <button type="submit" className="submit">Generate</button> 
-          
-        </form>
+        <form onSubmit={this.submitHandler} className="form">
+          <div className="config">
+            <div className="input_field">
+              Model:
+              <select
+                name="model"
+                className="input-box"
+                value={model}
+                onChange={this.changeHandler}
+                dangerouslySetInnerHTML={{ __html: models }}
+              ></select>
+            </div>
 
+            <div className="input_field">
+              Steps:
+              <input
+                type="number"
+                name="num_inference_steps"
+                min="25"
+                max="999"
+                value={num_inference_steps}
+                onChange={this.changeHandler}
+                className="input-box"
+              />
+            </div>
+
+            <div className="input_field">
+              Seed:
+              <input
+                name="seed"
+                type="number"
+                className="input-box"
+                value={seed}
+                onChange={this.changeHandler}
+              />
+            </div>
+            <div className="input_field">
+              Guidence scale:
+              <input
+                type="number"
+                name="guidance_scale"
+                className="input-box"
+                value={guidance_scale}
+                onChange={this.changeHandler}
+              />
+            </div>
+          </div>
+          <br></br>
+
+          <div className="ask">
+            <textarea
+              type="text"
+              className="question"
+              name="prompt"
+              value={prompt}
+              onChange={this.changeHandler}
+              required
+            />
+            <div className="result">
+              <img src={result} alt="Your generated frame here" />
+            </div>
+          </div>
+          <br></br>
+
+          <button type="submit" className="submit">
+            Generate
+          </button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default ImageGen
+export default ImageGen;
